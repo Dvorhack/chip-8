2401c2b5450fc98782e3678835814ddccce93061
Dvorhack <fcplm49@gmail.com>
2023-06-22 17:54:20 +0200

    loading rom

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,15 +1,18 @@
+use sdl2::libc::rand;
+use rand::Rng;
+use std::fs;
 
 
 const CPU_RAM: usize = 4096;
 const CPU_REGS: usize = 16;
 const CPU_STACK: usize = 48;
-const CHIP8_WIDTH: usize = 64;
-const CHIP8_HEIGHT: usize = 32;
+pub const CHIP8_WIDTH: usize = 64;
+pub const CHIP8_HEIGHT: usize = 32;
 
 pub struct CPU {
     ram: [u8; CPU_RAM],
     stack: [u16; CPU_STACK],
-    vmem: [[u8; CHIP8_WIDTH]; CHIP8_HEIGHT],
+    pub vmem: [[u8; CHIP8_WIDTH]; CHIP8_HEIGHT],
 
     regs: [u8; CPU_REGS],
     i: u16,
@@ -19,7 +22,8 @@ pub struct CPU {
 
     delay_timer: u8,
     sound_timer: u8,
-    input: [bool; 16]
+    input: [bool; 16],
+    pub draw_flag: bool
 }
 
 impl CPU {
@@ -42,14 +46,38 @@ impl CPU {
             delay_timer: 0,
             sound_timer: 0,
             input: [false; 16],
+            draw_flag: false
         }
     }
 
-    pub fn tick(&mut self, keys: [bool; 16]) -> bool{
+    pub fn tick(&mut self, keys: [bool; 16]){
         self.input = keys;
         self.pc += 1;
 
-        true
+        if self.pc == 500+0x200 {
+            
+            for x in 0..CHIP8_HEIGHT {
+                for y in 0..CHIP8_WIDTH {
+                    self.vmem[x][y] = rand::thread_rng().gen::<u8>() % 2 ;
+                }
+            }
+            self.draw_flag = true;
+            self.pc = 0x200;
+        }
 
+    }
+
+    pub fn load_game(&mut self, path :&str){
+        println!("Loading game {path}");
+        let data = fs::read(path).unwrap();
+        for (i, &byte) in data.iter().enumerate() {
+            let addr = 0x200 + i;
+            if addr < 4096 {
+                self.ram[0x200 + i] = byte;
+            } else {
+                break;
+            }
+        }
+        //println!("{:?}", &self.ram[0x200..0x200+10]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,39 @@ extern crate sdl2;
 mod cpu;
 
 use sdl2::pixels::Color;
+use sdl2::pixels;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::render::Canvas;
+use sdl2::rect::Rect;
+use sdl2::video::Window;
 use std::thread;
+use std::process::exit;
 
 use cpu::CPU;
+use cpu::{CHIP8_HEIGHT, CHIP8_WIDTH};
 
-fn setupGraphics(sdl_context: &sdl2::Sdl) {
+const SCALE_FACTOR: u32 = 10;
+
+fn setup_graphics(sdl_context: &sdl2::Sdl) -> Canvas<Window> {
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("rust-sdl2 demo", CHIP8_WIDTH as u32 *SCALE_FACTOR, CHIP8_HEIGHT as u32*SCALE_FACTOR)
         .position_centered()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.set_draw_color(Color::RGB(0, CHIP8_HEIGHT.try_into().unwrap(), CHIP8_WIDTH.try_into().unwrap()));
     canvas.clear();
     canvas.present();
+
+    canvas
 }
 
-fn getKeypad(sdl_context: &sdl2::Sdl) -> Result<[bool; 16], ()>{
+fn get_keypad(sdl_context: &sdl2::Sdl) -> Result<[bool; 16], ()>{
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
@@ -33,6 +43,7 @@ fn getKeypad(sdl_context: &sdl2::Sdl) -> Result<[bool; 16], ()>{
             Event::Quit {..} |
             Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                 println!("End");
+                exit(0);
             },
             _ => {}
         }
@@ -40,21 +51,47 @@ fn getKeypad(sdl_context: &sdl2::Sdl) -> Result<[bool; 16], ()>{
     Ok([true; 16])
 }
 
+fn color(value: u8) -> pixels::Color {
+    if value == 0 {
+        pixels::Color::RGB(0, 0, 0)
+    } else {
+        pixels::Color::RGB(0, 250, 0)
+    }
+}
+
+fn draw_screen(canvas: &mut Canvas<Window>, pixels: [[u8; CHIP8_WIDTH]; CHIP8_HEIGHT]) {
+    for (y, row) in pixels.iter().enumerate() {
+        for (x, &col) in row.iter().enumerate() {
+            let x = (x as u32) * SCALE_FACTOR;
+            let y = (y as u32) * SCALE_FACTOR;
+
+            canvas.set_draw_color(color(col));
+            let _ = canvas
+                .fill_rect(Rect::new(x as i32, y as i32, SCALE_FACTOR, SCALE_FACTOR));
+        }
+    }
+    canvas.present();
+}
+
 pub fn main() {
     let sleep_duration = Duration::from_millis(2);
 
     let sdl_context = sdl2::init().unwrap();
-    setupGraphics(&sdl_context);
+    let mut canvas = setup_graphics(&sdl_context);
 
     let mut cpu = CPU::new();
+    cpu.load_game("games/PONG");
     
     
-    while let Ok(keys) = getKeypad(&sdl_context) {
+    while let Ok(keys) = get_keypad(&sdl_context) {
 
-        let output = cpu.tick(keys);
+        cpu.tick(keys);
 
-        if output {
+        if cpu.draw_flag {
             // display_driver.draw(output.vram);
+            println!("Drawing");
+            draw_screen(&mut canvas, cpu.vmem);
+            cpu.draw_flag = false;
         }
 
         // if output.beep {
